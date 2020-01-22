791f22e1b21d2a76d115a87476fe637e8d6f89f3
wildintu <hello@wildintu.com>
2020-01-22 16:43:03 -0600

    Page Loaded

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
 let paragraph = document.createElement('p');
 let text = document.createTextNode('this can whatever text you would like it to be');
 
-paragraph.appendChild(text);
 paragraph.style.color = 'red';
 paragraph.style.textTransform = 'uppercase';
-paragraph.className = 'grid-square';
+paragraph.className = 'some-paragraph';
+
+paragraph.appendChild(text);
+document.body.appendChild(paragraph);
+})
