8aa170bb8310244fcf83a77efe88d566570f275a
wildintu <hello@wildintu.com>
2020-01-22 16:30:42 -0600

    paragraph color style classname

--- scripts.js
+++ scripts.js
@@ -2,3 +2,6 @@ let paragraph = document.createElement('p');
 let text = document.createTextNode('this can whatever text you would like it to be');
 
 paragraph.appendChild(text);
+paragraph.style.color = 'red';
+paragraph.style.textTransform = 'uppercase';
+paragraph.className = 'grid-square';
