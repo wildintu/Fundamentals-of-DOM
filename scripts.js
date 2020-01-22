a5b289c258b17f6307b98951494b13c4d12e47d5
wildintu <hello@wildintu.com>
2020-01-22 16:49:59 -0600

    added button and text

--- scripts.js
+++ scripts.js
@@ -8,4 +8,16 @@ paragraph.className = 'some-paragraph';
 
 paragraph.appendChild(text);
 document.body.appendChild(paragraph);
+
+let button = document.createElement('button');
+let btnText = document.createTextNode('Click Me!');
+button.appendChild(btnText);
+document.body.appendChild(button);
+
+button.addEventListener("click", function(){
+    let h1 = document.createElement('h1');
+    let h1Text = document.createTextNode("I've been clicked!");
+    h1.appendChild(h1Text);
+    document.body.appendChild(h1);
+})
 })
